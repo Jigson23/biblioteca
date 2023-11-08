97164b82221ad71d6170c610c4485e634717c715
Jigson23 <33557994+Jigson23@users.noreply.github.com>
2023-11-07 22:33:40 -0500

    Add clase Libro

---
+++ Readme.md
@@ -0,0 +1,10 @@
+Proyecto: Sistema de Gestion de Libros para una Biblioteca
+
+Objetivos del proyecto:
+
+1- Apreder a modelar la clases
+2- Menejar los colecciones Arralist
+3- Implemantar operacione CRUD
+4- Desarrollar una interfaz de usuario
+
+
---
+++ src/modelo/Libro.java
@@ -0,0 +1,64 @@
+package modelo;
+
+import java.util.ArrayList;
+
+public class Libro {
+    private String titulo;
+    private String autor;
+    private String ISBN;
+    private boolean prestado;
+    private ArrayList<String> tipoLibro;
+
+    public Libro() {
+    }
+
+    // Constructor
+    public Libro(String titulo, String autor, String ISBN, boolean prestado, ArrayList<String> tipoLibro) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ISBN = ISBN;
+        this.prestado = prestado;
+        this.tipoLibro = tipoLibro;
+    }
+
+    // Getters y Setters
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public boolean isPrestado() {
+        return prestado;
+    }
+
+    public void setPrestado(boolean prestado) {
+        this.prestado = prestado;
+    }
+
+    public ArrayList<String> getTipoLibro() {
+        return tipoLibro;
+    }
+
+    public void setTipoLibro(ArrayList<String> tipoLibro) {
+        this.tipoLibro = tipoLibro;
+    }
+}
