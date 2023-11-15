99090b921d24fc260589261b7d0704c045e44fb6
Jigson23 <33557994+Jigson23@users.noreply.github.com>
2023-11-14 23:36:37 -0500

    agregue fucion ramdon

---
+++ src/util/Util.java
@@ -0,0 +1,21 @@
+package util;
+
+import java.util.Random;
+
+public class Util {
+
+    public static String randomAlphanumericString(int length) {
+        String alphanumericCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuv";
+
+        StringBuffer randomString = new StringBuffer(length);
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(alphanumericCharacters.length());
+            char randomChar = alphanumericCharacters.charAt(randomIndex);
+            randomString.append(randomChar);
+        }
+
+        return randomString.toString();
+    }
+}
