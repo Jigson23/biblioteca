bbddd88e094e96ec67fd4931df3c5d76daa5a27d
Jigson23 <33557994+Jigson23@users.noreply.github.com>
2023-11-14 23:37:05 -0500

    intancia de objetos principales

--- src/SistemaBiblioteca.java
+++ src/SistemaBiblioteca.java
@@ -1,8 +1,11 @@
+import control.BibliotecaController;
+import modelo.Biblioteca;
+import vista.MenuPrincipal;
+
 public class SistemaBiblioteca {
-
-
-
     public static void main(String[] args) {
-
+        BibliotecaController controller = new BibliotecaController(new Biblioteca());
+        MenuPrincipal menuPrincipal = new MenuPrincipal(controller);
+        menuPrincipal.menu();
     }
 }
